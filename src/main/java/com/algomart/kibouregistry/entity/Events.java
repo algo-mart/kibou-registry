c695720f1ff6ac7ee17480166af9c3dfa1bbe974
Alao Abdulhakeema <alaoabdulhakeem55@gmail.com>
2024-03-12 01:11:31 +0100

    feat: set up database for backend development

---
+++ src/main/java/com/algomart/kibouregistry/entity/Events.java
@@ -0,0 +1,36 @@
+package com.algomart.kibouregistry.entity;
+import com.algomart.kibouregistry.enums.EventType;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+import java.util.List;
+@Data
+@Entity
+@Table(name = "events")
+public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long eventId;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "event_type")
+    private EventType eventType;
+
+    @Column(name = "venue")
+    private String venue;
+
+    @OneToOne
+    @JoinColumn(name = "payment_id")
+    private Payments payment;
+
+    @OneToOne
+    @JoinColumn(name = "attendance_id")
+    private Attendance attendance;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    private List<Participants> participants;
+}
