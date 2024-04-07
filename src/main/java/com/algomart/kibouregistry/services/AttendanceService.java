b1eca3b74022b21c55494c72daa01301aed65428
Alao Abdulhakeema <alaoabdulhakeem55@gmail.com>
2024-04-07 12:11:55 +0100

    feat: Create Endpoints for Recording Attendance

---
+++ src/main/java/com/algomart/kibouregistry/services/AttendanceService.java
@@ -0,0 +1,19 @@
+package com.algomart.kibouregistry.services;
+
+import com.algomart.kibouregistry.entity.Attendance;
+import com.algomart.kibouregistry.entity.Participants;
+import com.algomart.kibouregistry.response.APIResponse;
+
+public interface AttendanceService {
+
+    APIResponse recordAttendance(Attendance attendance);
+
+    APIResponse getAllAttendance(int pageSize, int pageNumber);
+
+    APIResponse getAttendanceByParticipantId(Participants participantId);
+
+    APIResponse updateAttendance(Long id, Attendance attendance);
+
+    APIResponse deleteAttendance(Long id);
+
+}
