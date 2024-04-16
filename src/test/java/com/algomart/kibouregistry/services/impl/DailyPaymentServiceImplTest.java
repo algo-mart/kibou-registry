5bba3af52281d7fcb6d1fa3b8dff906c0da12264
Alao Abdulhakeema <alaoabdulhakeem55@gmail.com>
2024-04-16 12:39:04 +0100

    test: Test Endpoints for Daily Payments implementations

--- src/test/java/com/algomart/kibouregistry/services/impl/DailyPaymentServiceImplTest.java
+++ src/test/java/com/algomart/kibouregistry/services/impl/DailyPaymentServiceImplTest.java
@@ -23,6 +23,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 public class DailyPaymentServiceImplTest {
     @Mock
