e36b26d91d4206326922bf2771aaf7b43468d3f5
Alao Abdulhakeema <alaoabdulhakeem55@gmail.com>
2024-04-16 09:24:16 +0100

    test: Test Endpoints for participants operations

--- src/test/java/com/algomart/kibouregistry/services/impl/ParticipantsServiceImplTest.java
+++ src/test/java/com/algomart/kibouregistry/services/impl/ParticipantsServiceImplTest.java
@@ -12,13 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Collections;
 import java.util.Optional;
