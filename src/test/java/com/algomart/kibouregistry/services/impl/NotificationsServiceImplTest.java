cf83fa383c0d86418a97787dfb81a4e1425f7689
Alao Abdulhakeema <alaoabdulhakeem55@gmail.com>
2024-04-22 21:33:53 +0100

    test: test NotificationsServiceImplTest

--- src/test/java/com/algomart/kibouregistry/services/impl/NotificationsServiceImplTest.java
+++ src/test/java/com/algomart/kibouregistry/services/impl/NotificationsServiceImplTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.*;
 
 import java.util.Arrays;
@@ -28,6 +29,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
 public class NotificationsServiceImplTest {
 
     @Mock
