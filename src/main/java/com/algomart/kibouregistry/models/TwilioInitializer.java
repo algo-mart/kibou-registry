598d055890d8838c61272a87ee52e5796181862f
Alao Abdulhakeema <alaoabdulhakeem55@gmail.com>
2024-04-22 22:57:47 +0100

    feat: Twilio and Mail configurations

--- src/main/java/com/algomart/kibouregistry/models/TwilioInitializer.java
+++ src/main/java/com/algomart/kibouregistry/models/TwilioInitializer.java
@@ -6,23 +6,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class TwilioInitializer {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(TwilioInitializer.class);
 
-    private  final TwilioConfig twilioConfig;
+    private final TwilioConfig twilioConfig;
 
     @Autowired
     public TwilioInitializer(TwilioConfig twilioConfig) {
         this.twilioConfig = twilioConfig;
-        Twilio.init(
-                twilioConfig.getAccountSid(),
-                twilioConfig.getAuthToken()
-        );
-        LOGGER.info("Twilio initialized ... with account sid {} ", twilioConfig.getAccountSid());
-
+        try {
+            Twilio.init(
+                    twilioConfig.getAccountSid(),
+                    twilioConfig.getAuthToken()
+            );
+            LOGGER.info("Twilio initialized with account sid: {}", twilioConfig.getAccountSid());
+        } catch (Exception e) {
+            LOGGER.error("Error initializing Twilio: {}", e.getMessage());
+        }
     }
-
 }
