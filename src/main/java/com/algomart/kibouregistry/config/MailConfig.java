ccb00f5173ea75e2c2aadb6b2e616e9859f1fe9b
Alao Abdulhakeema <alaoabdulhakeem55@gmail.com>
2024-04-26 09:37:55 +0100

    feat: refactor MailConfig class

--- src/main/java/com/algomart/kibouregistry/config/MailConfig.java
+++ src/main/java/com/algomart/kibouregistry/config/MailConfig.java
@@ -1,5 +1,8 @@
 package com.algomart.kibouregistry.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,20 +11,30 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
+@ConfigurationProperties(prefix = "mail")
+@Getter
+@Setter
 public class MailConfig {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587); // or your SMTP port
-        mailSender.setUsername("alaoabdulhakeem55@gmail.com");
-        mailSender.setPassword("bjrq ujqo gpyc ypql");
+        mailSender.setHost(this.host);
+        mailSender.setPort(this.port);
+        mailSender.setUsername(this.username);
+        mailSender.setPassword(this.password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.socketFactory.port", this.port);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.debug", "true");
 
         return mailSender;
