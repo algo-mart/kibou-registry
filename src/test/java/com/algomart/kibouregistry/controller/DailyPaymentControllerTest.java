069c05568c11a1e12ca7cf1e69f2e7d6c48268bd
AkinfalaOlaide <107960722+AkinfalaOlaide@users.noreply.github.com>
2024-09-18 06:11:04 -0700

    feat changed on daily payments controller tests again

--- src/test/java/com/algomart/kibouregistry/controller/DailyPaymentControllerTest.java
+++ src/test/java/com/algomart/kibouregistry/controller/DailyPaymentControllerTest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class DailyPaymentControllerTest {
+ class DailyPaymentControllerTest {
 
     @LocalServerPort
     private int port;
