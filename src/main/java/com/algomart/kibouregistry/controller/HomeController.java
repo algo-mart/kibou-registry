d17b9b29e51004a7097d868a10bd6a233c1fb246
Oloruntobi600 <tobiajayi60@gmail.com>
2025-01-07 14:38:56 +0100

    feat: security config

---
+++ src/main/java/com/algomart/kibouregistry/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.algomart.kibouregistry.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> HomeController(){
+        return new ResponseEntity<>("Welcome to Kibou-Project", HttpStatus.OK);
+
+    }
+}
