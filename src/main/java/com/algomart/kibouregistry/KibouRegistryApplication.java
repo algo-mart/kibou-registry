03bc2650889a400461ef1859280da47b8eb7c596
Oloruntobi600 <tobiajayi60@gmail.com>
2025-01-07 14:49:28 +0100

    feat: environments settings

--- src/main/java/com/algomart/kibouregistry/KibouRegistryApplication.java
+++ src/main/java/com/algomart/kibouregistry/KibouRegistryApplication.java
@@ -1,12 +1,20 @@
 package com.algomart.kibouregistry;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KibouRegistryApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./") // Ensure this is the directory where .env is located
+                .load();
+
+        // Optionally, print out the loaded variables for debugging
+        System.out.println("Database URL: " + dotenv.get("SPRING_DATASOURCE_URL"));
+
         SpringApplication.run(KibouRegistryApplication.class, args);
     }
 
