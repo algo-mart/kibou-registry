49a10325f27d916e7c3c4c66b716bcdbfac1060c
Oloruntobi600 <tobiajayi60@gmail.com>
2025-03-20 07:32:51 +0100

    feat:implement-getevent-byid-endpoint

--- src/main/java/com/algomart/kibouregistry/repository/EventsRepo.java
+++ src/main/java/com/algomart/kibouregistry/repository/EventsRepo.java
@@ -2,9 +2,18 @@ package com.algomart.kibouregistry.repository;
 import com.algomart.kibouregistry.entity.Events;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 public interface EventsRepo extends JpaRepository<Events, Long> , JpaSpecificationExecutor<Events> {
+
+    @Query("SELECT e FROM Events e LEFT JOIN FETCH e.users WHERE e.eventId = :id")
+    Optional<Events> findByIdWithUsers(@Param("id") Long id);
+
 
 
 }
